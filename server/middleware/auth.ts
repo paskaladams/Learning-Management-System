6d06ed625a538ab783a8ed5280c21192a2a69f36
Paskal Adams <paskaladams@gmail.com>
2024-01-25 14:38:44 +0700

    feat: push 2

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -6,8 +6,6 @@ import jwt, { JwtPayload } from 'jsonwebtoken';
 import { redis } from '../utils/redis';
 import { updateAccessToken } from '../controllers/user.controller';
 
-
-
 // Authenticated user 1
 // export const isAuthenticated = CatchAsyncError(async (req: Request, res: Response, next:NextFunction) => {
 // 	const access_token = req.cookies.access_token as string;
@@ -36,38 +34,34 @@ import { updateAccessToken } from '../controllers/user.controller';
 // Authenticated user 2
 interface ExtendedRequest extends Request {
 	user?: any;
-  }
+}
 
-export const isAuthenticated = CatchAsyncError(
-	async (req: ExtendedRequest, res: Response, next: NextFunction) => {
-	  const access_token = req.cookies.access_token;
-	  if (!access_token) {
-		return next(new ErrorHandler("Please login to access", 400));
-	  }
-	  const decoded = jwt.decode(access_token) as JwtPayload | null;
-  
-	  if (!decoded) {
-		return next(new ErrorHandler("Access token is not valid", 400));
-	  }
-	  // Check if the access token is is expired
-	  if (decoded.exp && decoded.exp <= Date.now() / 1000) {
+export const isAuthenticated = CatchAsyncError(async (req: ExtendedRequest, res: Response, next: NextFunction) => {
+	const access_token = req.cookies.access_token;
+	if (!access_token) {
+		return next(new ErrorHandler('Please login to access', 400));
+	}
+	const decoded = jwt.decode(access_token) as JwtPayload | null;
+
+	if (!decoded) {
+		return next(new ErrorHandler('Access token is not valid', 400));
+	}
+	// Check if the access token is is expired
+	if (decoded.exp && decoded.exp <= Date.now() / 1000) {
 		try {
-		  await updateAccessToken(req, res, next);
+			await updateAccessToken(req, res, next);
 		} catch (error) {
-		  return next(error);
+			return next(error);
 		}
-	  } else {
+	} else {
 		const user = await redis.get(decoded.id);
 		if (!user) {
-		  return next(
-			new ErrorHandler("Please login to access this resource..!", 400)
-		  );
+			return next(new ErrorHandler('Please login to access this resource..!', 400));
 		}
 		req.user = JSON.parse(user);
 		next();
-	  }
 	}
-  );
+});
 
 // Authenticated user 3
 // export const isAuthenticated = CatchAsyncError(
@@ -99,9 +93,9 @@ export const isAuthenticated = CatchAsyncError(
 // Validate user role
 export const authorizeRoles = (...roles: string[]) => {
 	return (req: Request, res: Response, next: NextFunction) => {
-		if(!roles.includes(req.user?.role || '')) {
+		if (!roles.includes(req.user?.role || '')) {
 			return next(new ErrorHandler(`Role: ${req.user?.role} is not allowed to access this resource`, 403));
 		}
 		next();
-	}
-}
+	};
+};
